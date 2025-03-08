ecb62e9deec43e73c613f4d089a9f372da1a37d4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2025-03-08 13:16:47 -0500

    chore: add routes and influencer controller

---
+++ app/controllers/influencers_controller.rb
@@ -0,0 +1,29 @@
+class InfluencersController < ApplicationController
+  def index
+    @influencers = Influencer.all
+  end
+
+  def create
+    @influencer = Influencer.new(influencer_params)
+
+    respond_to do |format|
+      if @influencer.save
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append("influencers", partial: "influencers/influencer", locals: { influencer: influencer })
+        end
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def new
+    @influencer = Influencer.new
+  end
+
+  private
+
+  def influencer_params
+    params.expect(influencer: %i[external_id username fullname picture followers is_verified platform])
+  end
+end
